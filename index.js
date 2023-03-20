5d80a68e917d94d76588c45faef1a6317073ba2d
rahul9416 <rahul.goyal9416@gmail.com>
2023-03-20 12:40:43 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const connectToMongo = require('./db');
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 connectToMongo();
 
 const app = express()
 const port = process.env.PORT || 3500
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 app.use('/api/auth', require("./routes/auth"));
 app.use('/api/notes', require("./routes/Notes"));
